dfed3f9a8c56beaec4b0662deaa3f3aed61b5426
Vishu2213 <113273799+Vishu2213@users.noreply.github.com>
2022-10-20 08:54:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    addTodo();
+
+});
+
+function addTodo() {
+    const todoText = input.value;
+
+    if(todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todoText;
+
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+        });
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+        })
+
+        todos.appendChild(todoEl);
+
+
+        input.value = "";
+    }
+}
+
